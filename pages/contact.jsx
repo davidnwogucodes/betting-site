f6801544adce155719d82b5b0f5179cac911598f
classic-code <amos.daniel@fiddoo.com>
2020-08-13 16:51:25 +0100

    update

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,32 +1,31 @@
 import React from "react";
-import style from "../styles/contact.module.scss";
 
 export default function contact() {
   return (
-    <div className={style.contactbody}>
-      <div className={style.contactmain}>
-        <div className={style.contacttext}>
+    <div className="contactbody">
+      <div className="contactmain">
+        <div className="contacttext">
           <h1>Hola, contact us</h1>
-          <div className={style.content}>
-            <div className={style.address}>
+          <div className="content">
+            <div className="address">
               <h3>
                 514 5, magnolia St.<p>Oriando.fl 32B06</p>
                 <p>California</p>
               </h3>
             </div>
-            <div className={style.contactemail}>
+            <div className="contactemail">
               <h3>hiagency@gmail.com</h3>
               <p>+1 900 77 4321</p>
             </div>
           </div>
-          <div className={style.socialmedia}>
-            <div className={style.instagram}>
+          <div className="socialmedia">
+            <div className="instagram">
               <h4>Instagram</h4>
             </div>
-            <div className={style.twitter}>
+            <div className="twitter">
               <h4>twitter</h4>
             </div>
-            <div className={style.facebook}>
+            <div className="facebook">
               <h4>facebook</h4>
             </div>
           </div>
